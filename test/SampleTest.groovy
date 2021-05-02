b41785eafe3ec2e9109f0d2e278ce7fcbfb1b08a
houdini91 <mdstrauss91@gmail.com>
2021-05-02 16:51:02 +0300

    Test

---
+++ test/SampleTest.groovy
@@ -0,0 +1,29 @@
+import org.junit.BeforeClass
+import org.junit.Test
+import sample
+
+class SampleTest {
+
+    /**
+     * Use Groovy metaclass programming to add methods to the Jenkins pipeline shared library exposed class.
+     * This allows for unit testing of classes that makes use of Jenkins pipeline steps, such as
+     * 'sh', 'echo' or e.g. other steps available through the workflow-basic-steps-plugin
+     */
+    @BeforeClass
+    static void setup() {
+        sample.metaClass.echo {
+            println it
+            return it
+        }
+    }
+
+    @Test
+    void shouldOutputSampleInformation() {
+        def varsFile = new sample()
+        def expectedArg = 'Mikey strauss'
+        def returnVal = varsFile.call(expectedArg)
+        assert returnVal != null
+        assert returnVal == "Project maintained by $expectedArg"
+    }
+
+}
