1c60bc48953cc27e826aef6fecd2f671a7ae497b
houdini91 <mdstrauss91@gmail.com>
2021-06-06 14:35:32 +0300

    Change name

--- vars/sendSamples.groovy
+++ vars/sendSamples.groovy
@@ -22,8 +22,6 @@ def sendSamples(String recipients, String attachment="samples.tar") {
 
 def call(String recipients="scribe-samples@scribesecurity.com", String dir="samples", String tar="samples.tar") {
     echo "Sending sample to $recipients"
-    // writeFile file:'scribe-groovy-html.template', text:libraryResource("scribe-groovy-html.template")
     def attachment = tar_samples(dir,tar)
     sendSamples(recipients, attachment)
-    // sh 'rm -rf scribe-groovy-html.template'
 }
