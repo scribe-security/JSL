93ff097e948bae23afb634c34953e745c48f8d20
houdini91 <mdstrauss91@gmail.com>
2021-06-06 14:14:05 +0300

    Change name

--- vars/sendSamples.groovy
+++ vars/sendSamples.groovy
@@ -12,20 +12,19 @@ def tar_samples(String dir, String tar="samples.tar"){
 
 def sendSamples(String recipients, String attachment="samples.tar") {
     def jobName = currentBuild.fullDisplayName
-    emailext body: '''${SCRIPT, template="scribe-groovy-html.template"}''',
+    emailext body: '''${SCRIPT, template="groovy-html.template"}''',
         attachLog: true,
         attachmentsPattern: attachment,
         mimeType: 'text/html',
         subject: "[Scribe] sample ${jobName}",
         to: "${recipients}",
-        replyTo: "${recipients}",
         recipientProviders: [[$class: 'CulpritsRecipientProvider']]
 }
 
 def call(String recipients="scribe-samples@scribesecurity.com", String dir="samples", String tar="samples.tar") {
     echo "Sending sample to $recipients"
-    writeFile file:'scribe-groovy-html.template', text:libraryResource("scribe-groovy-html.template")
+    // writeFile file:'scribe-groovy-html.template', text:libraryResource("scribe-groovy-html.template")
     def attachment = tar_samples(dir,tar)
     sendSamples(recipients, attachment)
-    sh 'rm -rf scribe-groovy-html.template'
+    // sh 'rm -rf scribe-groovy-html.template'
 }
