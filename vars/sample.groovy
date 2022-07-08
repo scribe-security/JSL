1360b246dea7ac4a3118827d9e0d11764608a7f9
houdini91 <mdstrauss91@gmail.com>
2022-07-08 12:16:51 +0300

    Test

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -49,85 +49,85 @@ def call(Map conf) {
     command.add("--verbose")
     command.add(conf.verbose.toString())
     
-    // if (config != "") {
-    //     command.add("--config")
-    //     command.add(conf.config)
-    // }
+    if (config != "") {
+        command.add("--config")
+        command.add(conf.config)
+    }
 
-    // if (format != "") {
-    //     command.add("--format")
-    //     command.add(conf.format)
-    // }
-    // if (output_directory != "") {
-    //     command.add("--output-directory")
-    //     command.add(output_directory)
-    // }
-    // if (output_file != "") {
-    //     command.add("--output-file")
-    //     command.add(output_file)
-    // }
-    // if (name != "") {
-    //     command.add("--name")
-    //     command.add(name)
-    // }
-    //     //     String[] env = [],
-    //     // String[] label  = [],
+    if (conf.format != "") {
+        command.add("--format")
+        command.add(conf.format)
+    }
+    if (conf.output_directory != "") {
+        command.add("--output-directory")
+        command.add(conf.output_directory)
+    }
+    if (conf.output_file != "") {
+        command.add("--output-file")
+        command.add(conf.output_file)
+    }
+    if (conf.name != "") {
+        command.add("--name")
+        command.add(conf.name)
+    }
+        //     String[] env = [],
+        // String[] label  = [],
 
-    // if (filter_regex != "") {
-    //     command.add("--filter-regex")
-    //     command.add(filter_regex)
-    // }
-    // if (collect_regex != "") {
-    //     command.add("--collect-regex")
-    //     command.add(collect_regex)
-    // }
+    if (conf.filter_regex != "") {
+        command.add("--filter-regex")
+        command.add(conf.filter_regex)
+    }
+    if (conf.collect_regex != "") {
+        command.add("--collect-regex")
+        command.add(conf.collect_regex)
+    }
 
-    // if (force) {
-    //     command.add("-f")
-    // }
+    if (conf.force) {
+        command.add("-f")
+    }
 
-    // if (attest_config != "") {
-    //     command.add("--attest.config")
-    //     command.add(attest_config)
-    // }
-    // if (attest_name != "") {
-    //     command.add("--attest.name")
-    //     command.add(attest_name)
-    // }
-    // if (attest_default != "") {
-    //     command.add("--attest.default")
-    //     command.add(attest_default)
-    // }
-    // if (collect_regex != "") {
-    //     command.add("--collect-regex")
-    //     command.add(collect_regex)
-    // }    
+    if (conf.attest_config != "") {
+        command.add("--attest.config")
+        command.add(conf.attest_config)
+    }
+    if (conf.attest_name != "") {
+        command.add("--attest.name")
+        command.add(conf.attest_name)
+    }
+    if (conf.attest_default != "") {
+        command.add("--attest.default")
+        command.add(conf.attest_default)
+    }
+    if (conf.collect_regex != "") {
+        command.add("--collect-regex")
+        command.add(conf.collect_regex)
+    }    
 
-    // if (scribe_enable) {
-    //     command.add("-E")
-    // }
+    if (conf.scribe_enable) {
+        command.add("-E")
+    }
 
-    // if (scribe_url != "") {
-    //     command.add("--scribe.url")
-    //     command.add(scribe_url)
-    // }    
-    // if (scribe_loginurl != "") {
-    //     command.add("--scribe.loginurl")
-    //     command.add(scribe_loginurl)
-    // }
-    // if (scribe_audience != "") {
-    //     command.add("--scribe.auth0.audience")
-    //     command.add(scribe_audience)
-    // }  
-    // if (context_dir != "") {
-    //     command.add("--context_dir")
-    //     command.add(context_dir)
-    // }
+    if (conf.scribe_url != "") {
+        command.add("--scribe.url")
+        command.add(conf.scribe_url)
+    }    
+    if (conf.scribe_loginurl != "") {
+        command.add("--scribe.loginurl")
+        command.add(conf.scribe_loginurl)
+    }
+    if (conf.scribe_audience != "") {
+        command.add("--scribe.auth0.audience")
+        command.add(conf.scribe_audience)
+    }  
+    if (conf.context_dir != "") {
+        command.add("--context_dir")
+        command.add(conf.context_dir)
+    }
 
-    // command.add("--context-type")
-    // command.add("jenkins")
+    command.add("--context-type")
+    command.add("jenkins")
    
-    // def commandStr = command.join(' ')
+    def commandStr = command.join(' ')
     echo "Command  - $command"
     // def out_data = sh(script: "${commandStr}",returnStdout: true)
     // println out_data
