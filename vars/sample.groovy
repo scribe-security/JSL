8cae5c69e572b54c382f1bdf58499795d22d93e9
houdini91 <mdstrauss91@gmail.com>
2022-07-08 12:21:49 +0300

    Test

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -49,81 +49,81 @@ def call(Map conf) {
     command.add("--verbose")
     command.add(conf.verbose.toString())
     
-    if (conf.config != "") {
+    if (conf.config != null ) {
         command.add("--config")
         command.add(conf.config)
     }
 
-    if (conf.format != "") {
+    if (conf.format != null) {
         command.add("--format")
         command.add(conf.format)
     }
 
     command.add("--output-directory")
-    if (conf.output_directory != "") {
+    if (conf.output_directory != null) {
         command.add(conf.output_directory)
     } else {
         command.add("scribe/bomber")
     }
 
-    if (conf.output_file != "") {
+    if (conf.output_file != null) {
         command.add("--output-file")
         command.add(conf.output_file)
     }
-    if (conf.name != "") {
+    if (conf.name != null) {
         command.add("--name")
         command.add(conf.name)
     }
         //     String[] env = [],
         // String[] label  = [],
 
-    if (conf.filter_regex != "") {
+    if (conf.filter_regex != null) {
         command.add("--filter-regex")
         command.add(conf.filter_regex)
     }
-    if (conf.collect_regex != "") {
+    if (conf.collect_regex != null) {
         command.add("--collect-regex")
         command.add(conf.collect_regex)
     }
 
-    if (conf.force) {
+    if (conf.force == true ) {
         command.add("-f")
     }
 
-    if (conf.attest_config != "") {
+    if (conf.attest_config != null) {
         command.add("--attest.config")
         command.add(conf.attest_config)
     }
-    if (conf.attest_name != "") {
+    if (conf.attest_name != null ) {
         command.add("--attest.name")
         command.add(conf.attest_name)
     }
-    if (conf.attest_default != "") {
+    if (conf.attest_default != null ) {
         command.add("--attest.default")
         command.add(conf.attest_default)
     }
-    if (conf.collect_regex != "") {
+    if (conf.collect_regex != null ) {
         command.add("--collect-regex")
         command.add(conf.collect_regex)
     }    
 
-    if (conf.scribe_enable) {
+    if (conf.scribe_enable == true) {
         command.add("-E")
     }
 
-    if (conf.scribe_url != "") {
+    if (conf.scribe_url != null) {
         command.add("--scribe.url")
         command.add(conf.scribe_url)
     }    
-    if (conf.scribe_loginurl != "") {
+    if (conf.scribe_loginurl != null ) {
         command.add("--scribe.loginurl")
         command.add(conf.scribe_loginurl)
     }
-    if (conf.scribe_audience != "") {
+    if (conf.scribe_audience != null ) {
         command.add("--scribe.auth0.audience")
         command.add(conf.scribe_audience)
     }  
-    if (conf.context_dir != "") {
+    if (conf.context_dir != null ) {
         command.add("--context_dir")
         command.add(conf.context_dir)
     }
