89617cdde40582bfb51694998ac3457a22d3c1a2
houdini91 <mdstrauss91@gmail.com>
2022-07-08 13:26:58 +0300

    Test

--- vars/bom.groovy
+++ vars/bom.groovy
@@ -2,36 +2,6 @@
 import java.nio.file.Path
 import java.nio.file.Paths
 
-def FindFiles(String directory, String ext="json") {
-    def files
-    dir (directory) {
-      files = findFiles(glob: """**/*.${ext}""")
-    }
-    return files
-}
-
-def PublishReport(String name="scribe", String directory="scribe") {
-    def file_list = []
-    try {
-      for (f in  FindFiles(directory)) {
-        if (! f.directory) {
-          echo """Publishing ${f.name} ${f.path} ${f.directory} ${f.length} ${f.lastModified}"""
-          file_list.add(f.path)
-        }
-      }
-
-      publishHTML (target : [allowMissing: false,
-          alwaysLinkToLastBuild: true,
-          keepAll: true,
-          reportDir: directory,
-          reportFiles: file_list.join(','),
-          reportName: name])
-    }
-    catch (Exception e) {
-      echo 'Exception occurred: ' + e.toString()
-    }
-}
-
 def call(Map conf) {
     def command = []
     
